1213aaeabefe8da04fd2eb3190133b544abca11e
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-31 16:10:40 -0400

    Prompt written.

--- CPP-PRG-09-08-Mode-Function/main.cpp
+++ CPP-PRG-09-08-Mode-Function/main.cpp
@@ -5,11 +5,23 @@
 //  Created by Keith Smith on 10/31/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  In statistics, the mode of a set of values is the value that occurs most often or with the
+//  greatest frequency. Write a function that accepts as arguments the following:
+//
+//      A) An array of integers
+//      B) An integer that indicates the number of elements in the array
+//
+//  The function should determine the mode of the array. That is, it should determine
+//  which value in the array occurs most often. The mode is the value the function
+//  should return. If the array has no mode (none of the values occur more than once),
+//  the function should return -1. (Assum the array will always contain nonnegative
+//  values.)
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
     return 0;
 }
