7e227ceb892c1b2037086aaeacd83412c828f90a
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-31 17:23:26 -0400

    Tested and working! Nice.

--- CPP-PRG-09-08-Mode-Function/main.cpp
+++ CPP-PRG-09-08-Mode-Function/main.cpp
@@ -21,7 +21,170 @@
 
 using namespace std;
 
+int *sizeArray();
+int *fillArray(int *, int);
+int *sortArrayNumbers(int *, int *, int);
+int *generateArrayTally(int *, int);
+void testForNoModeCase(int *, int);
+void displayArrays(const int *, const int *, int);
+
 int main()
 {
+    
+    int *intPtrArrIntegers = nullptr;
+    int *intPtrArrNumberSorted = nullptr;
+    int *intPtrArrNumberTally = nullptr;
+    
+    int *intSizeArray = nullptr;
+    
+    intSizeArray = sizeArray();
+    
+    intPtrArrIntegers = fillArray(intPtrArrIntegers, *intSizeArray);
+    
+    intPtrArrNumberTally = generateArrayTally(intPtrArrIntegers, *intSizeArray);
+    
+    testForNoModeCase(intPtrArrNumberTally, *intSizeArray);
+    
+    intPtrArrNumberSorted = sortArrayNumbers(intPtrArrNumberTally, intPtrArrIntegers, *intSizeArray);
+
+    cout << "The mode of that set is: " << intPtrArrNumberSorted[0] << endl;
+    
+    cout << "Number sorted: " << endl;
+    for (int i = 0 ; i < *intSizeArray ; i++)
+    {
+        cout << *(intPtrArrNumberSorted + i) << endl;
+    }
+    cout << endl;
+    
+//    cout << "Tally sorted: " << endl;
+//    for (int i = 0 ; i < *intSizeArray ; i++)
+//    {
+//        cout << *(intPtrArrNumberTally + i) << endl;
+//    }
+//    cout << endl;
+    
     return 0;
+}
+
+int *sizeArray()
+{
+    int *intSize =  nullptr;
+    
+    intSize = new int;
+    
+    cout << "Please enter the size of the array: ";
+    cin >> *intSize;
+    
+    return intSize;
+}
+
+int *fillArray(int *intArrIntegers, int intArrSize)
+{
+    intArrIntegers = new int[intArrSize];
+    
+    for (int i = 0 ; i < intArrSize ; i++)
+    {
+        cout << "Please enter value " << (i + 1) << " of " << (intArrSize) << ": ";
+        cin >> intArrIntegers[i];
+    }
+    
+    return intArrIntegers;
+}
+
+int *generateArrayTally(int *intArrIntegers, int intArrSize)
+{
+    int *intArrSorted = nullptr;
+    int *intArrTally = nullptr;
+    
+    intArrSorted = new int[intArrSize];
+    intArrTally = new int[intArrSize];
+    
+    for (int i = 0 ; i < intArrSize ; i++)
+    {
+        intArrSorted[i] = intArrIntegers[i];
+        intArrTally[i] = 0;
+    }
+    
+    for (int i = 0 ; i < intArrSize ; i++)
+    {
+        for (int j = 0 ; j < intArrSize ; j++)
+        {
+            if (intArrIntegers[i] == intArrSorted[j])
+            {
+                intArrSorted[j] = intArrIntegers[i];
+                intArrTally[j]++;
+                break;
+            }
+        }
+    }
+    
+    return intArrTally;
+}
+
+int *sortArrayNumbers(int *intArrTally, int *intArrIntegers, int intArrSize)
+{
+    int intStart;
+    int intMinIndex;
+    
+    int *fltMinElement = nullptr;
+    
+    int *strMinElement = nullptr;
+    
+    for(intStart = 0 ; intStart < (intArrSize - 1) ; intStart++)
+    {
+        intMinIndex = intStart;
+        fltMinElement = &intArrTally[intStart];
+        
+        for(int index = (intStart + 1) ; index < intArrSize ; index++)
+        {
+            if(intArrTally[index] > *fltMinElement)
+            {
+                fltMinElement = &intArrTally[index];
+                strMinElement = &intArrIntegers[index];
+                intMinIndex = index;
+            }
+        }
+        swap(intArrTally[intMinIndex], intArrTally[intStart]);
+        swap(intArrIntegers[intMinIndex], intArrIntegers[intStart]);
+    }
+    
+    cout << "Tally sorted: " << endl;
+    for (int i = 0 ; i < intArrSize ; i++)
+    {
+        cout << *(intArrTally + i) << endl;
+    }
+    cout << endl;
+    
+    return intArrIntegers;
+}
+
+void testForNoModeCase(int *intArrTally, int intArrSize)
+{
+    bool booAreAllSame = false;
+    
+    for(int i = 0 ; i < intArrSize ; i++)
+    {
+        if(intArrTally[i] != 1)
+        {
+            booAreAllSame = false;
+        }
+        else
+        {
+            booAreAllSame = true;
+        }
+    }
+    
+    if(booAreAllSame == true)
+    {
+        cout << "There is no mode in that set." << endl;
+        exit(0);
+    }
+}
+
+void displayArrays(const int *intArr1, const int *intArr2, int intArrSize)
+{
+    for (int i = 0 ; i < intArrSize ; i++)
+    {
+        
+    }
 }
